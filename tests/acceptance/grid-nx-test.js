8fafc5e716b76e9bfaa1d85149c34cc101ec29c5
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2014-10-14 11:21:00 +0000

    acceptance tests

--- tests/acceptance/grid-nx-test.js
+++ tests/acceptance/grid-nx-test.js
@@ -12,10 +12,68 @@ module('Acceptance: GridNx', {
   }
 });
 
-test('visiting /grid-nx', function() {
+test('should respect default sort', function() {
+  expect(1);
+
   visit('/artists');
 
   andThen(function() {
-    equal(currentPath(), 'artists.index');
+    equal(findWithAssert('.grid-nx table tbody td:first').text(), '1');
   });
 });
+
+test('should reorder on header click', function() {
+  expect(1);
+
+  visit('/artists');
+  click('.grid-nx table thead th:first');
+
+  andThen(function() {
+    equal(findWithAssert('.grid-nx table tbody td:first').text(), '5');
+  });
+});
+
+test('should have the same quantity on reorder', function() {
+  expect(1);
+
+  visit('/artists');
+  click('.grid-nx table thead th:first');
+
+  andThen(function() {
+    equal(findWithAssert('.grid-nx table tbody tr').length, 5);
+  });
+});
+
+test('should find valid results', function() {
+  expect(1);
+
+  visit('/artists');
+  fillIn('.grid-nx input', 'bl');
+
+  andThen(function() {
+    equal(findWithAssert('.grid-nx table tbody tr').length, 2);
+  });
+});
+
+test('should search on all valid query attributes', function() {
+  expect(3);
+
+  visit('/artists');
+
+  fillIn('.grid-nx input', 'm');
+  andThen(function() {
+    equal(findWithAssert('.grid-nx table tbody tr').length, 2);
+  });
+
+  fillIn('.grid-nx input', '1968');
+  andThen(function() {
+    equal(findWithAssert('.grid-nx table tbody tr').length, 1);
+  });
+
+  fillIn('.grid-nx input', '19');
+  andThen(function() {
+    equal(findWithAssert('.grid-nx table tbody tr').length, 5);
+  });
+});
+
+
--- tests/dummy/app/artist/model.js
+++ tests/dummy/app/artist/model.js
@@ -9,7 +9,9 @@ Artist.reopenClass({
   FIXTURES: [
     {id:1, name:'Alice In Chains', year:1987},
     {id:2, name:'Black Label Society', year:1998},
-    {id:3, name:'Black Sabbath', year:1968}
+    {id:3, name:'Black Sabbath', year:1968},
+    {id:4, name:'Metallica', year:1981},
+    {id:5, name:'Mad Season', year:1994}
   ]
 });
 
